7d41f45d72b56fb07ea15772744d7175b55c7578
Strochini <0.618@bk.ru>
2022-11-09 03:24:59 +0300

    HW4 1 lesson

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
+        var firstBoxerWeight = 78.2;
+        System.out.println("First boxer weight " + firstBoxerWeight);
+        var secondBoxerWeight = 82.7;
+        System.out.println("Second boxer Weight " + secondBoxerWeight);
+
+        var bothBoxersWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println("Both boxers weight " + bothBoxersWeight);
+
+        var bothBoxersWeightDifference = secondBoxerWeight - firstBoxerWeight;
+        System.out.println("Both boxers weight difference " + bothBoxersWeightDifference);
+
+        var weightDifference = secondBoxerWeight % firstBoxerWeight;
+        System.out.println("Weight difference " + weightDifference);
+
+
+
     }
 }
